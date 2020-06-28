6f5956ccc6cf68a011c9df40c62898953a4e2b3f
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-06-28 14:03:16 +0900

    feat: add example

---
+++ src/PInvokeExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using static PInvoke.User32;
+
+namespace PInvokeExample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            EnumWindows((hWnd, param) =>
+            {
+                var length = GetWindowTextLength(hWnd);
+                if (length > 0)
+                {
+                    var str = new char[length + 1];
+                    GetWindowText(hWnd, str, str.Length);
+
+                    Console.WriteLine(str);
+                }
+
+                return true;
+            }, IntPtr.Zero);
+
+            Console.ReadLine();
+        }
+    }
+}
